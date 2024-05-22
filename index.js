057d3a4e2a6081dd62cd445859192bcf2988871d
as-vittsivskyi <147527745+as-vittsivskyi@users.noreply.github.com>
2024-05-22 13:07:37 +0000

    upd pr

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
 
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-module.exports = app;
+module.exports = { app, server };
--- tests/app.test.js
+++ tests/app.test.js
@@ -1,11 +1,15 @@
-// tests/app.test.js
 const request = require('supertest');
-const app = require('../index');
+const { app, server } = require('../index');
 
-describe('GET /', () => {
+describe('GET /hello', () => {
   it('responds with Hello World!', async () => {
-    const res = await request(app).get('/');
+    const res = await request(app).get('/hello');
     expect(res.statusCode).toEqual(200);
     expect(res.text).toEqual('Hello World!');
+  });
+  
+  afterAll(async () => {
+    // Закриваємо сервер після завершення всіх тестів
+    await server.close();
   });
 });
